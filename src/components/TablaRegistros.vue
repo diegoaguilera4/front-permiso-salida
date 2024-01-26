5a79021530f873332e390902bc94982dc4667707
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-26 10:07:39 -0300

    filtros centro de costo, cargo y tipo funcionales en registros

--- src/components/TablaRegistros.vue
+++ src/components/TablaRegistros.vue
@@ -11,15 +11,61 @@
         append-inner-icon="mdi-magnify"
       ></v-text-field>
     </v-col>
+    <v-col>
+      <v-select
+        v-model="costoFilter"
+        clearable
+        chips
+        label="Centro de costo"
+        :items="getUniqueValues('d_cencos')"
+        append-inner-icon="mdi-domain"
+        height="20"
+      ></v-select>
+    </v-col>
+    <v-col>
+      <v-select
+        v-model="cargoFilter"
+        clearable
+        chips
+        label="Cargo"
+        :items="getUniqueValues('d_cargo')"
+        append-inner-icon="mdi-account-tie"
+        height="20"
+      ></v-select>
+    </v-col>
+    <v-col>
+      <v-select
+        v-model="tipoFilter"
+        clearable
+        chips
+        label="Tipo"
+        :items="getUniqueValues('tipo')"
+        append-inner-icon="mdi-account-tie"
+        height="20"
+      ></v-select>
+    </v-col>
   </v-row>
   <!-- Tabla de registros -->
-  <v-data-table :headers="headers" :items="filteredRegistros">
+  <v-data-table
+    :headers="headers"
+    :items="filteredRegistros"
+    style="margin-top: 20px; text-align: left"
+    items-per-page-text="Registros por página"
+    page-text="{0}-{1} de {2}"
+  >
     <template v-slot:top>
       <!-- Barra de herramientas -->
       <v-toolbar flat>
         <v-toolbar-title>Registros</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
+        <v-btn
+          variant="tonal"
+          append-icon="mdi-file-excel-outline"
+          style="margin-right: 10px;"
+        >
+          Exportar
+        </v-btn>
         <v-btn
           variant="tonal"
           append-icon="mdi-update"
@@ -51,11 +97,24 @@ export default {
       { title: "Fecha y hora", key: "fechaHoraAux" },
       { title: "Tipo", key: "tipo" },
     ],
+    costoFilter: null,
+    cargoFilter: null,
+    tipoFilter: null,
   }),
 
   computed: {
     filteredRegistros() {
-      return this.registros.filter((item) => this.searchFilter(item));
+      return this.registros.filter((item) => {
+        return (
+          (this.searchFilter(item) || this.search === "") &&
+          (this.costoFilter === null ||
+            this.costoFilter === "" ||
+            item.d_cencos === this.costoFilter) &&
+          (this.cargoFilter === null ||
+            this.cargoFilter === "" ||
+            item.d_cargo === this.cargoFilter) && (this.tipoFilter === null || this.tipoFilter === "" || item.tipo === this.tipoFilter)
+        );
+      });
     },
   },
 
@@ -85,7 +144,18 @@ export default {
       var segundos = fechaOriginal.substring(17, 19);
 
       // Formatear la fecha en el nuevo formato
-      var fechaFormateada = dia + "-" + mes + "-" + año + " " + horas + ":" + minutos + ":" + segundos;
+      var fechaFormateada =
+        dia +
+        "-" +
+        mes +
+        "-" +
+        año +
+        " " +
+        horas +
+        ":" +
+        minutos +
+        ":" +
+        segundos;
       return fechaFormateada;
     },
     actualizarRegistros() {
@@ -97,18 +167,28 @@ export default {
           "http://localhost:3000/registros/obtenerTodos"
         );
         if (response.data) {
-            this.registros = response.data;
+          this.registros = response.data;
           //formatear fechas
-            for (var i = 0; i < this.registros.length; i++) {
-                this.registros[i].fechaHoraAux = this.formatFecha(this.registros[i].fechaHora);
-            }
-            
+          for (var i = 0; i < this.registros.length; i++) {
+            this.registros[i].fechaHoraAux = this.formatFecha(
+              this.registros[i].fechaHora
+            );
+          }
         } else {
           console.error("La respuesta no contiene datos válidos.");
         }
       } catch (error) {
         console.error("Error al obtener los registros:", error.message);
       }
+    },
+    getUniqueValues(columnKey) {
+      const uniqueValues = new Set();
+      this.filteredRegistros.forEach((item) => {
+        if (item[columnKey]) {
+          uniqueValues.add(item[columnKey]);
+        }
+      });
+      return Array.from(uniqueValues);
     },
     searchFilter(item) {
       return Object.values(item).some(
